32d8198d8d13a098cf6bdbf2faae2606f83a6fb5
Vihrogonov <vihrogonov@protonmail.com>
2021-11-12 18:19:10 +0000

    Connect to Contentful & Adding .ENV

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,10 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   /* Your site config here */
   siteMetadata: {
@@ -29,6 +33,13 @@ module.exports = {
         path: `${__dirname}/src/assets/images`,
       },
     },
-    
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `bsdo09r1cdah`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_API_KEY,
+      },
+    },
   ],
 }
