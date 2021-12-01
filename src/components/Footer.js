5ba2de7bf5d895e018ae2e1d5ef1025614be3a69
Vihrogonov <vihrogonov@protonmail.com>
2021-12-01 11:01:01 +0000

    Adding recipes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,10 @@ const Footer = () => {
     <footer className="page-footer">
       <p>
         &copy; {new Date().getFullYear()} <span>SimpleRecipes</span>. Build with
-        <a href="https://gatsbyjs.com"> Gatsby</a>
+        <a href="https://reactjs.org"> React</a>,
+        <a href="https://gatsbyjs.com"> Gatsby</a>,
+        <a href="https://graphql.org"> GraphQL</a>,
+        <a href="https://contentful.com"> Contentful</a>
       </p>
     </footer>
   )
