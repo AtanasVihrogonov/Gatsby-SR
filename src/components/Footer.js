d44afbec487cd7dcd058e8749523a28ea2a3cfc6
Vihrogonov <vihrogonov@protonmail.com>
2021-12-03 17:50:05 +0000

    Update Footer Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,15 @@ const Footer = () => {
   return (
     <footer className="page-footer">
       <p>
-        &copy; {new Date().getFullYear()} <span>SimpleRecipes</span>. Build with
-        <a href="https://reactjs.org"> React</a>,
-        <a href="https://gatsbyjs.com"> Gatsby</a>,
-        <a href="https://graphql.org"> GraphQL</a>,
-        <a href="https://contentful.com"> Contentful</a>
+        Build with
+        <a href="https://reactjs.org"> React</a> /
+        <a href="https://gatsbyjs.com"> Gatsby</a> /
+        <a href="https://graphql.org"> GraphQL</a> /
+        <a href="https://contentful.com"> Contentful CMS</a>
+      </p>
+      <p>
+        &copy; {new Date().getFullYear()} <span>SimpleRecipes. </span>All rights
+        reserved.
       </p>
     </footer>
   )
